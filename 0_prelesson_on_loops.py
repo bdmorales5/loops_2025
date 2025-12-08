824678401cd14d7668a78b5eabb0ba4896b7861c
bdmorales5 <bdmorales5@cps.edu>
2025-12-08 13:38:22 -0600

    Update 0_prelesson_on_loops.py

--- 0_prelesson_on_loops.py
+++ 0_prelesson_on_loops.py
@@ -1,2 +1,30 @@
-Loops video  https://www.youtube.com/watch?v=KWgYha0clzw&t=1s&pp=ygUOYnJvIGNvZGUgbG9vcHM%3D
-While loops  
+# Loops video  https://www.youtube.com/watch?v=KWgYha0clzw&t=1s&pp=ygUOYnJvIGNvZGUgbG9vcHM%3D
+  
+# for loops = execute a block of code a fixed number of times
+#     you can interate over a range , string, sequance, etc
+ 
+
+
+# for x in range (1,21):
+#     if x==13:
+#         break
+#     else:
+#         print(x)
+
+
+
+# while loops
+# while loops= execute some code WHILE some condition remains True
+# name = input("enter your name")
+# while name =="":
+#     print("you did not enter your name")
+#     name = input("enter your name")
+
+# print(f"hello {name}")
+
+# age = int(input("enter your age"))
+# while age < 0:
+#     print("age cant be negative")
+#     age = int(input("enter your age"))
+
+# print(f"you are {age} year old")
