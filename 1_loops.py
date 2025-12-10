42ca28503de7e97fe353555386c9de82e7bdce32
bdmorales5 <bdmorales5@cps.edu>
2025-12-10 12:31:27 -0600

    Update 1_loops.py

--- 1_loops.py
+++ 1_loops.py
@@ -4,23 +4,54 @@ fruits = ["apple", "banana", "cherry", "date"]
 
 # Challenge:
 # Use a for loop to print each fruit on a new line.
-# print(fruits[0])
-# print(fruits[1])
-# print(fruits[2])
+print(len(fruits))
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
 for fruit in fruits:
     print(fruit)
-# hop on fort
+# # i just worked with loops
+
 # Given a list of school subjects:
 subjects = ["Math", "Science", "History", "Art"]
 for subject in subjects:
     print(subject)
-# Challenge:
+Challenge:
 # Use a for loop and range to print each subject along with its index:
 # Example output: "Subject 0: Math"
+for subject in subject:
+    if subject == "history":
+        break
+    print(subject)
+    for subject in subject :
+        if subject == "science":
+            continue
+        print(subject)
+
+list1000 = list(range(1,1001))
+# break when you reach a number greather than 599
+for number in list1000:
+    if number > 599:
+        break 
+    print(number)
+
+for number in list1000:
+    if number > 500:
+        break
+    print(number)
+
+
+
+
+
+
+
+
+
 
 
 # Given:
-numbers = [5, 10, 15, 20]
+# numbers = [5, 10, 15, 20]
 
-# Challenge:
+# # Challenge:
 # Use a for loop to add all the numbers and print the total.
